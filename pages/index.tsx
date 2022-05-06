61315f5b3de0c7f8a6b562bf97e0efc56c1fd183
farrar142 <gksdjf1690@gmail.com>
2022-05-06 23:08:35 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+import type { NextPage } from "next";
+import React from "react";
+const Home: NextPage = (props) => {
+  return <Box></Box>;
+};
+export default Home;
